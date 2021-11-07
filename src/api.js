8f68521ac27648aa176dad422a1f1913d58c6923
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-11-07 21:40:13 +0800

    Change the baseURL to task-loopback api deployed

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: `http://localhost:4000`,
+	// baseURL: `http://localhost:4000`,
 	// baseURL: `https://vc-task.herokuapp.com/api`,
-	// baseURL: 'https://damp-sea-61499.herokuapp.com/'
+	baseURL: 'https://task-loopback.herokuapp.com/'
 
 	// baseURL: `http://localhost:5000/`
 });
